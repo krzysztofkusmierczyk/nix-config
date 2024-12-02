189f25deb76e2f5b7f38d22bdd09d8ca6439e81f
Krzysztof Kuśmierczyk <11976750+krzysztofkusmierczyk@users.noreply.github.com>
2024-12-02 16:51:17 +0100

    Create symlinkg for whole nvim dir.

--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -10,6 +10,10 @@
 
   config = lib.mkIf config.neovim.enable {
     home.packages = [pkgs.neovim];
-    xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home-manager/neovim/files/init.lua";
+
+    xdg.configFile."nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home-manager/neovim/files";
+      recursive = true;
+    };
   };
 }
