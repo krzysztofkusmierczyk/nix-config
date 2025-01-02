b69593358f7dbbae8ffaef09578724842ad1d168
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@outlook.com>
2025-01-02 16:34:03 +0100

    Update telescope config

--- modules/home-manager/neovim/nvim/lua/config/plugins/telescope.lua
+++ modules/home-manager/neovim/nvim/lua/config/plugins/telescope.lua
@@ -4,28 +4,53 @@ return {
     tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
       require("telescope").setup {
-        -- pickers = {
-        --   find_files = {
-        --     theme = "ivy"
-        --   }
-        -- },
-        extensions = {
-          fzf = {}
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "build",
+            "dist",
+            "target", ".git", ".next", ".yarn", ".swc", "coverag", }, },
+        pickers = {
+          find_files = {
+            -- theme = "ivy"
+            hidden = true,
+            no_ignore = true
+          }
         },
+        extensions = {
+          fzf = {},
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+          }
+        },
+
       }
+      -- TODO: Is this needed?
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
       local builtin = require 'telescope.builtin'
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind files" })
-      vim.keymap.set("n", "<leader>fn",
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set("n", "<leader>sn",
         function()
           builtin.find_files { cwd = vim.fn.stdpath("config") }
         end,
-        { desc = "[F]ind [N]vim config files" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind help tags" })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+        { desc = "[S]earch [N]eovim files" })
     end
   }
 }
