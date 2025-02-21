df5c9108dbb0f537c47878692f1ffe140e23ba4b
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@outlook.com>
2025-02-21 18:17:41 +0100

    Remove commented out code

--- modules/home-manager/neovim/nvim/init.lua
+++ modules/home-manager/neovim/nvim/init.lua
@@ -62,20 +62,5 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
--- Example for opening a small terminal
--- local job_id = 0
--- vim.keymap.set("n", "<leader>st", function()
---   vim.cmd.vnew()
---   vim.cmd.term()
---   vim.cmd.wincmd("J")
---   vim.api.nvim_win_set_height(0, 15)
---
---   job_id = vim.bo.channel
--- end)
--- -- Example of running a command in the small terminal
--- -- TODO: Look into having use commands like in vscode to run tests
--- vim.keymap.set("n", "<leader>rt", function()
---   vim.fn.chansend(job_id, { "make test\r\n" })
--- end)
 
 require("config.lazy")
